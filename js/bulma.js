f26896d9edf3f99f2f3c6374a2f4763e9c0fcf62
Daniel Supernault <danielsupernault@gmail.com>
2016-09-02 18:26:39 -0600

    Update skeleton template

---
+++ js/bulma.js
@@ -0,0 +1,10 @@
+// The following code is based off a toggle menu by @Bradcomp
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+(function() {
+    var burger = document.querySelector('.nav-toggle');
+    var menu = document.querySelector('.nav-menu');
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
