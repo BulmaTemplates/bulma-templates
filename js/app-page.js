5944e25d556203cbdf3ccd25ec9acf361eeec76a
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2021-12-09 04:00:38 -0600

    new template, app-page (#163)
    Co-authored-by: Weckmann <clark@weckmann.me>

---
+++ js/app-page.js
@@ -0,0 +1,17 @@
+/**
+ * Dropdown menu
+ */
+document.addEventListener('DOMContentLoaded', function() {
+    const menus = document.querySelectorAll('.navbar-burger');
+    const dropdowns = document.querySelectorAll('.navbar-menu');
+
+    if (menus.length && dropdowns.length) {
+        for (var i = 0; i < menus.length; i++) {
+            menus[i].addEventListener('click', function() {
+                for (var j = 0; j < dropdowns.length; j++) {
+                    dropdowns[j].classList.toggle('is-active');
+                }
+            });
+        }
+    }
+});
