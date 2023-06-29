ef8e162ad62e421d532e02489eef1cce7650ccdf
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2023-06-29 15:41:20 +0200

    Update cardsData.js

--- js/cardsData.js
+++ js/cardsData.js
@@ -11,7 +11,7 @@ let cardsData = [{
 },
 {
   id: 1,
-  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  image: 'https://images.unsplash.com/photo-1687851898832-650714860119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
   user: {
     name: 'Okinami',
@@ -21,8 +21,8 @@ let cardsData = [{
   content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
 },
 {
-  id: 1,
-  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  id: 2,
+  image: 'https://ucarecdn.com/77beffe4-d4aa-4a7b-a951-31e0dfe36e90',
   avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
   user: {
     name: 'Okinami',
@@ -32,8 +32,8 @@ let cardsData = [{
   content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
 },
 {
-  id: 1,
-  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  id: 3,
+  image: 'https://ucarecdn.com/77beffe4-d4aa-4a7b-a951-31e0dfe36e90',
   avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
   user: {
     name: 'Okinami',
@@ -43,8 +43,8 @@ let cardsData = [{
   content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
 },
 {
-  id: 1,
-  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  id: 4,
+  image: 'https://ucarecdn.com/77beffe4-d4aa-4a7b-a951-31e0dfe36e90',
   avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
   user: {
     name: 'Okinami',
@@ -54,8 +54,8 @@ let cardsData = [{
   content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
 },
 {
-  id: 1,
-  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  id: 5,
+  image: 'https://ucarecdn.com/77beffe4-d4aa-4a7b-a951-31e0dfe36e90',
   avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
   user: {
     name: 'Okinami',
@@ -65,8 +65,8 @@ let cardsData = [{
   content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
 },
 {
-  id: 1,
-  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  id: 6,
+  image: 'https://ucarecdn.com/83e71ac6-6c75-4e7c-96e9-5aae15a4b3f0/',
   avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
   user: {
     name: 'Okinami',
